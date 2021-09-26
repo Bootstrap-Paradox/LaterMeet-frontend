0a9b81d1e2289326fd6d5063f0405a61750b17e3
Sid Kuchnure <sidkuchnure@gmail.com>
2021-09-26 21:19:26 +0530

    🖥  Update View Meeting Page
    - OnReload Redirect -> dashboard home
    - New Support UI Components

--- src/Pages/dashboard/meetingView.js
+++ src/Pages/dashboard/meetingView.js
@@ -1,19 +1,43 @@
-import React, { useContext, useEffect, useState } from 'react';
-import useLocalStorage from '../../Hooks/useLocalStorage';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 import { SuperContext } from './dashboardHome';
 
 const MeetingView = () => {
 
     const { superState, superDispatch } = useContext(SuperContext)
+    const history = useHistory()
 
     let meetingData = superState.meetingData;
 
-
+    if (Object.keys(superState.meetingData).length === 0) history.push("/d/h")
 
 
     return (
         <>
-            <h1>{meetingData["meeting_name"]}</h1>
+            {Object.keys(meetingData).length > 0 &&
+                <section className="meeting-view">
+
+                    <h1 className="title">{meetingData["meeting_name"]}</h1>
+                    <div style={{ height: "208px" }} className="box view-box">
+                        <h5><span>Description</span></h5>
+                        <p>{meetingData["meeting_description"]}</p>
+                    </div>
+                    <div style={{ height: "208px" }} className="box view-box">
+                        <h5><span>Timings</span></h5>
+                        {meetingData["meeting_timings"].length > 0 ? meetingData["meeting_timings"].map(
+                            (timing, index) => {
+                                return (
+                                    <div key={index} className="box-tile">{timing}</div>
+                                )
+
+                            }
+                        ) :
+                            <h3 className="light-tip">On Join Load</h3>
+                        }
+                    </div>
+                    <button className="btn btn-secondary btn-long-xl">Edit Meeting</button>
+                </section>
+            }
         </>
     )
 }
